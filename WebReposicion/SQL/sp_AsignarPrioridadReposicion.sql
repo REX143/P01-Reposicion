1eea6c799685318de2ed73aac38ae79fd89510f0
Herberts Aldava Asencios <h.gaia@live.com>
2020-09-15 23:14:05 -0500

    Procedimiento almacedado asoiado al CUS - 05

---
+++ WebReposicion/SQL/sp_AsignarPrioridadReposicion.sql
@@ -0,0 +1,37 @@
+Create Procedure [dbo].[sp_AsignarPrioridadReposicion]
+AS
+-- Obtener el el stock actual para ver sus clasificaciones
+declare @tabla table(Pk int,ClasificacionABC varchar(10),ClasificacionXYZ varchar(10))
+insert into @tabla(Pk,ClasificacionABC,ClasificacionXYZ) select CodigoArticulo AS 'Pk',ClasificacionABC,ClasificacionXYZ from stock
+
+declare @count int=(select count(*) from @tabla) --Total de artículos a clasificar
+
+while @count>0 --5636
+begin
+		-- Variables para obtener las clasifiaciones ABC/XYZ por código
+		declare @CodigoArticulo int=(select top(1) Pk from @tabla order by Pk Desc)
+		declare @ClasificacionABC varchar(10)=(select top(1) ClasificacionABC from Stock where CodigoArticulo=@CodigoArticulo order by CodigoArticulo Desc)
+		declare @ClasificacionXYZ varchar(10)=(select top(1) ClasificacionXYZ from Stock where CodigoArticulo=@CodigoArticulo order by CodigoArticulo Desc)
+		declare @Matriz varchar(10)=@ClasificacionABC+@ClasificacionXYZ
+		declare @Prioridad varchar(15)
+	
+		SET @Prioridad= CASE @Matriz
+			 WHEN 'AX' THEN 'Prioridad 1'
+			 WHEN 'AY' THEN 'Prioridad 1'
+			 WHEN 'BX' THEN 'Prioridad 1'
+			 WHEN 'AZ' THEN 'Prioridad 2'
+			 WHEN 'BY' THEN 'Prioridad 2'
+			 WHEN 'BZ' THEN 'Prioridad 2'
+			 WHEN 'CX' THEN 'Prioridad 3'
+			 WHEN 'CY' THEN 'Prioridad 3'
+			 WHEN 'CZ' THEN 'Prioridad 4'
+		ELSE 'S/P'
+		END; 
+
+		update Stock set PrioridadReposicion=@Prioridad where CodigoArticulo=@CodigoArticulo
+	
+		delete @tabla where Pk=@CodigoArticulo
+		set @count=(select count(*) from @tabla) 
+end
+
+--Exec sp_AsignarPrioridadReposicion
