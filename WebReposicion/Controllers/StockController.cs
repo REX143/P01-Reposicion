b04cc5e327c3e2d6b7727d70da741f55e771b1cc
Herberts Aldava Asencios <h.gaia@live.com>
2020-09-17 03:26:32 -0500

    CUS-07 Consultar stock => Pruebas de concepto a través de este caso de uso

--- WebReposicion/Controllers/StockController.cs
+++ WebReposicion/Controllers/StockController.cs
@@ -3,15 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebReposicion.Models.ViewModels;
+using WebReposicion.Models;
 
 namespace WebReposicion.Controllers
 {
     public class StockController : Controller
     {
         // GET: Stock
-        public ActionResult Index()
+        
+        public ActionResult Index(string cadena)
         {
-            return View();
+            List<StockViewModel> articulos;
+
+            using (DBPREDICTIVOEntities db=new DBPREDICTIVOEntities())
+            {
+                if (cadena !="")
+                {
+                    articulos = (from d in db.sp_obtenerConsultaStock(cadena)
+                                 select new StockViewModel
+                                 {
+                                     Pk = d.Pk,
+                                     Codigo = d.Codigo,
+                                     Nombre = d.Nombre,
+                                     Categoria = d.Categoria,
+                                     Stock = d.Stock,
+                                     Comprometido = d.Comprometido,
+                                     Disponible = d.Disponible,
+                                     ABC = d.ABC,
+                                     XYZ = d.XYZ,
+                                     Predictivo = d.Predictivo,
+                                     Minimo = d.Minimo,
+                                     Maximo = d.Maximo,
+                                     PrioridadReposicion = d.PrioridadReposicion,
+                                     Ruta = d.Ruta
+
+                                 }).ToList();
+                             return View(articulos);
+                }
+
+                return Redirect("~/Home/Index");
+            }
+                
         }
     }
 }
---
+++ WebReposicion/Models/ViewModels/StockViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebReposicion.Models.ViewModels
+{
+    public class StockViewModel
+    {
+        public int Pk { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public Nullable<int> Stock { get; set; }
+        public Nullable<int> Comprometido { get; set; }
+        public Nullable<int> Disponible { get; set; }
+        public string ABC { get; set; }
+        public string XYZ { get; set; }
+        public Nullable<int> Predictivo { get; set; }
+        public Nullable<int> Minimo { get; set; }
+        public Nullable<int> Maximo { get; set; }
+        public string PrioridadReposicion { get; set; }
+        public string Ruta { get; set; }
+    }
+}
